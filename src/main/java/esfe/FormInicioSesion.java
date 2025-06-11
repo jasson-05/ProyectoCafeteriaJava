b06b29b27ee40a177d08c4902eff24662eadf0a5
Emily Lisbeth Bolaño Flores <emilyflores.093@gmail.com>
2025-06-10 21:14:41 -0600

    quite codigo que no era necesario en la clase del formInicioSesion

--- src/main/java/esfe/FormInicioSesion.java
+++ src/main/java/esfe/FormInicioSesion.java
@@ -8,10 +8,5 @@ public class FormInicioSesion {
     private JButton registrarseButton;
     private JFormattedTextField formattedTextField2;
 
-    public static void main(String[] args) {
-        // Ejecutar la GUI en el Event Dispatch Thread (EDT)
-        SwingUtilities.invokeLater(() -> {
-            new FormInicioSesion();
-        });
-    }
+
 }
