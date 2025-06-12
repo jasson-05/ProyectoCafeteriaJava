69845636cd395a524a1eb8da4e890af330177154
Walter Ivan Rosa Sandoval <ivanrosasandoval@gmail.com>
2025-06-12 08:09:21 -0600

    script actualizado

--- src/main/java/esfe/Persistencia/CafeteriaBD.sql
+++ src/main/java/esfe/Persistencia/CafeteriaBD.sql
@@ -3,22 +3,22 @@ CREATE DATABASE Cafeteria_BD
 USE Cafeteria_BD
 GO
 
-CREATE TABLE datosclientes(
-	iddcliente int IDENTITY(1,1) NOT NULL,
-	nombre nvarchar(50) NULL,
-	apellido nvarchar(50) NULL,
-	correo nvarchar(50) NULL
+CREATE TABLE Users(
+	id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	name VARCHAR (100) NULL,
+	passwordHash VARCHAR (64) NOT NULL,
+	email VARCHAR (200) NOT NULL UNIQUE,
+	status TINYINT NOT NULL
 )
 GO
 
 -- 'precio' ya es 'float' que es doble precisión en SQL Server
 CREATE TABLE menuss(
-	codpro int NOT NULL,
+	id_codpro INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
 	nompro varchar (40) NULL,
 	precio float NULL,
 	categoria varchar(40) NULL,
-	tipo varchar(40) NULL,
-	estado nchar(10) NULL
+	tipo varchar(40) NULL
 )
 GO
 
@@ -30,3 +30,23 @@ CREATE TABLE vent(
 	estado nchar(10) NULL,
 	fecha datetime NULL
 ) ;
+
+ALTER TABLE menuss
+ALTER COLUMN 	id INT PRIMARY KEY IDENTITY(1,1) NOT NULL;
+
+CREATE TABLE Employees (
+    id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    position VARCHAR(50) NULL,
+    salary FLOAT NULL,
+    phoneNumber VARCHAR(20) NULL,
+    addressemployess VARCHAR(255) NULL,
+   );
+
+
+   CREATE TABLE Orders (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    UserId INT NOT NULL,
+    TotalAmount DECIMAL(10,2) NOT NULL,
+    OrderDate DATETIME NOT NULL
+);
