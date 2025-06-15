5c5d4d22fef3d356579669c15cf36d1052f28a4a
Jasson Bladimir Navarro Giron <Jasso@DESKTOP-97AP7FC>
2025-06-15 16:35:21 -0600

    diformulario login sin diseño agregado a la carpeta presentacion

---
+++ src/main/java/esfe/presentacion/LoginForm.java
@@ -0,0 +1,11 @@
+package esfe.presentacion;
+
+import javax.swing.*;
+
+public class LoginForm {
+    private JTextField txtEmail;
+    private JPasswordField txtPassword;
+    private JPanel mainPael;
+    private JButton btnLogin;
+    private JButton btnSalir;
+}
